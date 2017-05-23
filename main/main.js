db6507f6fc973c22b8e323ed9a10ae96bc9fce78
bobobobbbb <1105388289@qq.com>
2017-05-23 23:48:06 +0800

    ..

--- main/main.js
+++ main/main.js
@@ -1,5 +1,10 @@
-// Write your cade below:
-module.exports = function main(a, b) {
-    var c = a % b;
-    return c;
+module.exports = function main(str) {
+  // Write your cade here
+     var len = str.length
+             var num = 0;
+         for (var i = 0; i < len; i ++) {
+               var currentnum =  parseInt(str.substr(i, 1))
+               num += currentnum
+             }
+             return num
 };
